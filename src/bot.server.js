459fce1b26473658c8dbc3709d970f4a19032a53
theusmon0v <asadbek.usmonoff2003@gmail.com>
2024-02-21 18:07:08 +0500

    contact yuborish yozildi

--- src/bot.server.js
+++ src/bot.server.js
@@ -6,7 +6,7 @@ const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/start/, msg => {
     const { id, first_name } = msg.from;
-    bot.sendMessage(id, `*Assalomu Alaykum* *${first_name}.* *Yo'lovchilar uchun xizmat ko'rsatuvchi botimizga xush kelibsiz!* 🙂\n\nBotning asosiy maqsadi. \n_Kitob ➡️ Toshkent | Toshkent ➡️ Kitob_ \n_Shaxrisabz ➡️ Toshkent_ \n_Toshkent ➡️ Shaxrisabz yo'lovchilari uchun xizmat ko'rsatishdir._\n\nMarhamat o'zingizga mos xizmat turini tanlashingiz mumkin!👇`, {
+    bot.sendMessage(id, `*Assalomu Alaykum* *${first_name}.* *Yo'lovchilar uchun xizmat ko'rsatuvchi botimizga xush kelibsiz!* 🙂\n\nBotning asosiy maqsadi. \n_Kitob ➡️ Toshkent | Toshkent ➡️ Kitob_ \n_Shaxrisabz ➡️ Toshkent_ \n_Toshkent ➡️ Shaxrisabz yo'lovchilari uchun xizmat ko'rsatishdir._\n\nMarhamat o'zingizga kerakli xizmat turini tanlashingiz mumkin!👇`, {
         parse_mode: 'Markdown',
         reply_markup: {
             keyboard: [
@@ -32,16 +32,44 @@ bot.on("message", (msg) => {
         })
     }
 })
-
 bot.on("message", (msg) => {
     const {id} = msg.chat
-    if(msg.text ==="Toshkent"){
-        bot.sendMessage(id, "Ismingizni yozing", {
-        })
-    } else if (msg.text === "Qashqadaryo"){
-        bot.sendMessage(id, "Ismingizni yozing", {
-        })
-    }
+    if(msg.text === "Orqaga ⬅️")
+    bot.sendMessage(id, "Marhamat o'zingizga kerakli xizmat turini tanlashingiz mumkin!👇",{
+        reply_markup: {
+            keyboard: [
+                [{text: "Yo'lovchi sifatida"}, {text: "Pochta yuborish"}],
+                [{text: "Buyurtmalarim"}]
+            ],
+            resize_keyboard: true
+        }
+    })
 })
+
+bot.on("message", (msg) => {
+    const { id } = msg.chat;
+    if (msg.text === "Toshkent" || msg.text === "Qashqadaryo") {
+        bot.sendMessage(id, "Ismingizni yozing ✍️", {
+            reply_markup: {
+                remove_keyboard: true
+            }
+        }).then(() => {
+            bot.once("message", (msg) => {
+                const { id } = msg.chat;
+                bot.sendMessage(id, "Telefon raqamingizni yuboring ✉️", {
+                    reply_markup: {
+                        keyboard: [[{ text: "Telefon raqamni yuborish", request_contact: true }]],
+                        resize_keyboard: true
+                    }
+                });
+            });
+        });
+    }
+});
+
+
+
+
+
 
 
