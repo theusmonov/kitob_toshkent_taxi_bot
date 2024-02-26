2681655d3e644c0e6aad3e91ede6c97878793bb5
theusmon0v <asadbek.usmonoff2003@gmail.com>
2024-02-26 12:22:40 +0500

    user malumot kiritganda hammasini bitta obj da yig'ib qaytarib ko'rildi

--- src/bot.server.js
+++ src/bot.server.js
@@ -4,8 +4,11 @@ import "dotenv/config.js";
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
+
+let userMalumot = {}
+
 bot.onText(/\/start/, msg => {
-    const { id, first_name } = msg.from;
+    const {id, first_name} = msg.from;
     bot.sendMessage(id, `*Assalomu Alaykum* *${first_name}.* *Yo'lovchilar uchun xizmat ko'rsatuvchi botimizga xush kelibsiz!* 🙂\n\nBotning asosiy maqsadi. \n_Kitob ➡️ Toshkent | Toshkent ➡️ Kitob_ \n_Shaxrisabz ➡️ Toshkent_ \n_Toshkent ➡️ Shaxrisabz yo'lovchilari uchun xizmat ko'rsatishdir._\n\nMarhamat o'zingizga kerakli xizmat turini tanlashingiz mumkin!👇`, {
         parse_mode: 'Markdown',
         reply_markup: {
@@ -20,7 +23,7 @@ bot.onText(/\/start/, msg => {
 
 bot.on("message", (msg) => {
     const {id} = msg.chat
-    if(msg.text ==="Yo'lovchi sifatida"){
+    if (msg.text === "Yo'lovchi sifatida") {
         bot.sendMessage(id, "Yo'lanishni tanlang!👇 \nQayerga bormoqchisiz?", {
             reply_markup: {
                 keyboard: [
@@ -34,21 +37,23 @@ bot.on("message", (msg) => {
 })
 bot.on("message", (msg) => {
     const {id} = msg.chat
-    if(msg.text === "Orqaga ⬅️")
-    bot.sendMessage(id, "Marhamat o'zingizga kerakli xizmat turini tanlashingiz mumkin!👇",{
-        reply_markup: {
-            keyboard: [
-                [{text: "Yo'lovchi sifatida"}, {text: "Pochta yuborish"}],
-                [{text: "Buyurtmalarim"}]
-            ],
-            resize_keyboard: true
-        }
-    })
+    if (msg.text === "Orqaga ⬅️")
+        bot.sendMessage(id, "Marhamat o'zingizga kerakli xizmat turini tanlashingiz mumkin!👇", {
+            reply_markup: {
+                keyboard: [
+                    [{text: "Yo'lovchi sifatida"}, {text: "Pochta yuborish"}],
+                    [{text: "Buyurtmalarim"}]
+                ],
+                resize_keyboard: true
+            }
+        })
 })
+
 
 bot.on("message", (msg) => {
     const { id } = msg.chat;
     if (msg.text === "Toshkent" || msg.text === "Qashqadaryo") {
+        userMalumot["Borish manzili"] = msg.text;
         bot.sendMessage(id, "Ismingizni yozing ✍️", {
             reply_markup: {
                 remove_keyboard: true
@@ -56,7 +61,7 @@ bot.on("message", (msg) => {
         }).then(() => {
             bot.once("message", (msg) => {
                 const { id } = msg.chat;
-                const ism = msg.text.trim();
+                userMalumot["Ismi"] = msg.text.trim();
                 bot.sendMessage(id, "Telefon raqamingizni yuboring, yoki aloqa uchun boshqa raqamni junatmoqchi bo'lsangiz uni yozing. Namuna: +998900010290 ✉️", {
                     reply_markup: {
                         keyboard: [[{ text: "Telefon raqamni yuborish", request_contact: true }]],
@@ -65,21 +70,27 @@ bot.on("message", (msg) => {
                 }).then(() => {
                     bot.once("message", (msg) => {
                         const { id } = msg.chat;
-                        const telRaqam = msg.text.trim();
-                        const uzRaqam = /^\+998[0-9]{9}$/;
-                        if (!uzRaqam.test(telRaqam)) {
-                            bot.sendMessage(id, "Xatolik! Iltimos, to'g'ri raqamni yuboring.", {
-                                reply_markup: {
-                                    remove_keyboard: true
-                                }
-                            });
+                        if (msg.contact) {
+                            userMalumot["Telefon"] = msg.contact.phone_number;
+                        } else {
+                            userMalumot["Telefon"] = msg.text.trim();
                         }
+                        let malumotString = "";
+                        for (const key in userMalumot) {
+                            if (userMalumot.hasOwnProperty(key)) {
+                                malumotString += `${key}: ${userMalumot[key]}\n`;
+                            }
+                        }
+                        bot.sendMessage(id, `Ma'lumotlar:\n${malumotString}`);
                     });
                 });
             });
         });
     }
 });
+
+
+
 
 
 
